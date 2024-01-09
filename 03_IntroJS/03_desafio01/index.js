ac07da01c8a0a05483fa4d423807cd5558f5da37
RenatoBaeza <rntbzbg@gmail.com>
2024-01-09 20:42:46 -0300

    intro js y desafio 1

---
+++ 03_IntroJS/03_desafio01/index.js
@@ -0,0 +1,14 @@
+let precio = 400000
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio
+
+
+let cantidad = 1
+cantidadSpan = document.querySelector(".cantidad");
+cantidadSpan.innerHTML = cantidad
+
+let valortotal = precio * cantidad
+valortotalSpan = document.querySelector(".valortotal");
+valortotalSpan.innerHTML = valortotal
+
+
