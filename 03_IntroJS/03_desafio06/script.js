921ade108098518da3dd78a8722580925e2598d5
RenatoBaeza <rntbzbg@gmail.com>
2024-02-15 19:15:34 -0300

    final desafio 6

--- 03_IntroJS/03_desafio06/script.js
+++ 03_IntroJS/03_desafio06/script.js
@@ -1,18 +1,43 @@
 async function obtenerIndicador() {
     const res = await fetch("https://mindicador.cl/api/");
     const data = await res.json();
-    console.log("This is the data", data);
-    return data; // Return the fetched data
+    console.log(data)
+    return data;
 }
 
-async function calcularCambio(event) {
-    event.preventDefault(); // Prevent form submission
-    const data = await obtenerIndicador(); // Await the API response
-    const pesosingresados = document.getElementById('amount').value;
-    if (data.euro && data.euro.valor) {
-        const nuevovalor = pesosingresados / data.euro.valor;
-        document.getElementById("conversionResult").innerHTML = nuevovalor.toFixed(2); // Update the result display
-    } else {
-        console.error("Euro data is not available");
-    }
+async function calcularCambio() {
+    const data = await obtenerIndicador();
+    const monto = document.getElementById('amount').value;
+    const moneda = document.getElementById('currency').value;
+    console.log(moneda)
+    const nuevovalor = monto / data[moneda].valor;
+    document.getElementById("resultado").innerHTML = nuevovalor.toFixed(2);
+}
+
+async function indicadorMes(moneda) {
+    const res_moneda = await fetch("https://mindicador.cl/api/" + moneda);
+    const data_moneda = await res_moneda.json();
+    console.log(data_moneda)
+    return data_moneda;
+}
+
+async function mostrarHistorialMoneda() {
+    const moneda = document.getElementById('currency').value;
+    const data_moneda = await indicadorMes(moneda);
+    const last10DaysData = data_moneda.serie.slice(-10);
+    const fechas = last10DaysData.map(item => new Date(item.fecha).toLocaleDateString());
+    const valores = last10DaysData.map(item => item.valor);
+    var ctx = document.getElementById('currencyChart').getContext('2d');
+    window.currencyChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: fechas,
+            datasets: [{
+                label: `Valor de ${data_moneda.nombre} los ultimos 10 dias`,
+                backgroundColor: 'green', 
+                borderColor: 'black',
+                data: valores
+            }]
+        }
+    });
 }
