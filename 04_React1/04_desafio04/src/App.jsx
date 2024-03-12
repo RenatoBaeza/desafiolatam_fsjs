e9f3fc5ce1f6f750753313d1f28fd56af378cc05
RenatoBaeza <rntbzbg@gmail.com>
2024-03-12 18:19:35 -0300

    update

--- 04_React1/04_desafio04/src/App.jsx
+++ 04_React1/04_desafio04/src/App.jsx
@@ -1,14 +1,21 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import MiApi from "./assets/components/MiApi";
+import Buscador from "./assets/components/Buscador";
 import { Container, Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  
   return (
-    <Container className="bg-danger mw-100 p-5">
+    <Container className="bg-primary mw-100 p-5">
       <Row className="justify-content-center ">
         <Col xs={12} md={6} className="p-4 border rounded bg-light">
-          <MiApi />
+          <h1>Game Info</h1>
+          <p>Usa la API de https://www.cheapshark.com/api/1.0/games para buscar el precio más barato para juegos.</p>
+          <Buscador setSearchTerm={setSearchTerm} />
+          <MiApi searchTerm={searchTerm} />
         </Col>
       </Row>
     </Container>
--- 04_React1/04_desafio04/src/assets/components/MiApi.jsx
+++ 04_React1/04_desafio04/src/assets/components/MiApi.jsx
@@ -1,11 +1,51 @@
-/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
 
+const MiApi = ({ searchTerm }) => {
+    const [games, setGames] = useState([]);
 
-const MiApi = () => {
+    useEffect(() => {
+        const fetchGameData = async () => {
+            try {
+                const response = await fetch(`https://www.cheapshark.com/api/1.0/games?title=${encodeURIComponent(searchTerm)}`);
+                const data = await response.json();
+                setGames(data);
+            } catch (error) {
+                console.error("Error fetching data: ", error);
+            }
+        };
+
+        if (searchTerm) {
+            fetchGameData();
+        }
+    }, [searchTerm]);
+
     return (
-    <>
-
-    </>
+        <div className='m-2'>
+            <h2>Lista de juegos</h2>
+            {games.length > 0 ? (
+                <Table striped bordered hover size="sm">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Game Title</th>
+                            <th>Cheapest Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {games.map((game, index) => (
+                            <tr key={game.gameID}>
+                                <td>{index + 1}</td>
+                                <td>{game.external}</td>
+                                <td>${game.cheapest}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            ) : (
+                <p>No games found</p>
+            )}
+        </div>
     );
 };
 
