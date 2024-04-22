f8970bd69554987d453ac7ed66f44ff3a2b56cf2
RenatoBaeza <rntbzbg@gmail.com>
2024-04-22 19:58:30 -0400

    desafio3 final

--- 05_React2/05_desafio03/src/assets/views/PokemonDetail.jsx
+++ 05_React2/05_desafio03/src/assets/views/PokemonDetail.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
 
 function PokemonDetail() {
-    const { name } = useParams();
+    const navigate = useNavigate();
+    const {name} = useParams();
     const [pokemonDetails, setPokemonDetails] = useState({});
 
     useEffect(() => {
@@ -11,12 +14,31 @@ function PokemonDetail() {
         .then(data => setPokemonDetails(data));
     }, [name]);
 
+    const goToPokemonDetails = () => { navigate(`/pokemon/`); }
+
     return (
         <div>
-            <h1>{pokemonDetails.name}</h1>
+            <h1 class="text-danger">{pokemonDetails.name}</h1>
             <img src={pokemonDetails.sprites?.front_default} alt={name} />
-            <p>Height: {pokemonDetails.height}</p>
-            <p>Weight: {pokemonDetails.weight}</p>
+            <table class="table table-light table-hover table-sm">
+                <thead>
+                    <tr>
+                    <th scope="col">Stat</th>
+                    <th scope="col">Value</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr><th scope="row">Height</th><td>{pokemonDetails.height}</td></tr>
+                    <tr><th scope="row">Weight</th><td>{pokemonDetails.weight}</td></tr>
+                    <tr><th scope="row">HP</th><td>{pokemonDetails.weight}</td></tr>
+                    <tr><th scope="row">Attack</th><td>{pokemonDetails.weight}</td></tr>
+                    <tr><th scope="row">Defense</th><td>{pokemonDetails.weight}</td></tr>
+                    <tr><th scope="row">Sp. Attack</th><td>{pokemonDetails.weight}</td></tr>
+                    <tr><th scope="row">Sp. Defense</th><td>{pokemonDetails.weight}</td></tr>
+                    <tr><th scope="row">Speed</th><td>{pokemonDetails.weight}</td></tr>
+                </tbody>
+            </table>
+            <Button className='bg-danger m-2' onClick={goToPokemonDetails}>Volver</Button>
         </div>
     );
 }
