702c9846dfdde049a1bcc84b3763e8e77b8d26a3
RenatoBaeza <rntbzbg@gmail.com>
2024-04-29 18:06:46 -0400

    final desafio 4

--- 05_React2/05_desafio04/src/assets/views/Carrito.jsx
+++ 05_React2/05_desafio04/src/assets/views/Carrito.jsx
@@ -1,11 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import React, { useEffect, useState, useContext } from "react";
+import { Card, Row, ListGroup, Button } from "react-bootstrap";
+import { PizzaContext } from "../context/PizzaContext";
 
 const Carrito = () => {
+    const { carrito, setCarrito } = useContext(PizzaContext);
+
     return (
         <div>
-
+            <h5 className="">Detalle del pedido:</h5>
+            
+            <Row xs={2} md={5} className="m-2">
+                {carrito.map(carrito => (
+                    <>
+                        <p>{carrito.name}</p>
+                        <p>{carrito.amount}</p>
+                    </>
+                ))}
+            </Row>
         </div>
     );
 };
