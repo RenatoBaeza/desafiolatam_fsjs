34bf6843928a47abbe37b5d4f740e55411559919
RenatoBaeza <rntbzbg@gmail.com>
2024-06-29 14:01:50 -0400

    desafio 3 finished

--- 07_node/07_desafio03/server/consultas.js
+++ 07_node/07_desafio03/server/consultas.js
@@ -1,26 +1,24 @@
 const { Pool } = require('pg')
+
 const pool = new Pool({
     host: 'localhost',
     user: 'postgres',
-    password: 'postgres',
+    password: '5eiq7aog',
     database: 'likeme',
     allowExitOnIdle: true
 })
 
-const getDate = async () => {
-    const result = await pool.query("SELECT NOW()")
-    console.log(result)
-}
-
-const agregarViaje = async (destino, presupuesto) => {
-    const consulta = "INSERT INTO viajes values (DEFAULT, $1, $2)"
-    const values = [destino, presupuesto]
+const agregarPost = async (titulo, img, description, likes) => {
+    const consulta = "INSERT INTO posts values (DEFAULT, $1, $2, $3, $4)"
+    const values = [titulo, img, description, likes]
     const result = await pool.query(consulta, values)
-    console.log("Viaje agregado")
+    console.log("Post agregado")
 }
 
-const obtenerViajes = async () => {
-    const { rows } = await pool.query("SELECT * FROM viajes")
+const obtenerPost = async () => {
+    const { rows } = await pool.query("SELECT * FROM posts")
     console.log(rows)
     return rows
 }
+
+module.exports = { agregarPost, obtenerPost }
--- 07_node/07_desafio03/server/server.js
+++ 07_node/07_desafio03/server/server.js
@@ -1,51 +1,25 @@
+const { agregarPost, obtenerPost } = require('./consultas')
 const express = require('express');
 const fs = require('fs');
+const cors = require('cors')
 const app = express();
 const PORT = 3000;
-
-app.use(express.json());
-app.use(express.static('public'));
-
-let rawData = fs.readFileSync('repertorio.json');
-let songs = JSON.parse(rawData);
-
-app.post('/canciones', (req, res) => {
-    const newSong = req.body;
-    newSong.id = songs.length ? songs[songs.length - 1].id + 1 : 1;
-    songs.push(newSong);
-    fs.writeFileSync('repertorio.json', JSON.stringify(songs, null, 2));
-    res.status(201).send(newSong);
-});
-
-app.get('/canciones', (req, res) => {
-    res.json(songs);
-});
-
-app.put('/canciones/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const updatedSong = req.body;
-    const index = songs.findIndex(song => song.id === id);
-    if (index !== -1) {
-        songs[index] = { ...songs[index], ...updatedSong };
-        fs.writeFileSync('repertorio.json', JSON.stringify(songs, null, 2));
-        res.send(songs[index]);
-    } else {
-        res.status(404).send({ error: 'Cancion no encontrada' });
-    }
-});
-
-app.delete('/canciones/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const index = songs.findIndex(song => song.id === id);
-    if (index !== -1) {
-        const deletedSong = songs.splice(index, 1);
-        fs.writeFileSync('repertorio.json', JSON.stringify(songs, null, 2));
-        res.send(deletedSong);
-    } else {
-        res.status(404).send({ error: 'Cancion no encontrada' });
-    }
-});
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
 });
+
+app.use(cors())
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get("/posts", async (req, res) => {
+    const viajes = await obtenerPost()
+    res.json(viajes)
+    })
+
+app.post("/posts", async (req, res) => {
+    const { titulo, img, description, likes } = req.body
+    await agregarPost(titulo, img, description, likes)
+    res.send("Post agregado con éxito")
+    })
