92b0620b6144dca6dbf7faf599422f9d8c7c3c79
RenatoBaeza <rntbzbg@gmail.com>
2024-07-13 13:04:14 -0400

    desafio4 init

---
+++ 07_node/07_desafio04/server/index.js
@@ -0,0 +1,17 @@
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+
+import todoRoute from "./routes/todo.route.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use("/todos", todoRoute);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}`);
+});
