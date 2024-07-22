c9b5f677d5eeb41496d98730bb1fa754d7af2561
RenatoBaeza <rntbzbg@gmail.com>
2024-07-21 21:09:02 -0400

    final desafio 5

---
+++ 07_node/07_desafio05/server/consultas.js
@@ -0,0 +1,45 @@
+const { Pool } = require('pg');
+const bcrypt = require('bcryptjs');
+require('dotenv').config();
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    allowExitOnIdle: true
+});
+
+const registrarUsuario = async (usuario) => {
+    const { email, password, rol, lenguage } = usuario;
+    const passwordEncriptada = bcrypt.hashSync(password, 10);
+    const values = [email, passwordEncriptada, rol, lenguage];
+    const consulta = "INSERT INTO usuarios (email, password, rol, lenguage) VALUES ($1, $2, $3, $4)";
+    await pool.query(consulta, values);
+};
+
+const verificarCredenciales = async (email, password) => {
+    const values = [email];
+    const consulta = "SELECT * FROM usuarios WHERE email = $1";
+    const { rows: [usuario], rowCount } = await pool.query(consulta, values);
+    if (!rowCount) {
+        throw { code: 401, message: "Email o contraseña incorrecta" };
+    }
+    const { password: passwordEncriptada } = usuario;
+    const passwordEsCorrecta = bcrypt.compareSync(password, passwordEncriptada);
+    if (!passwordEsCorrecta) {
+        throw { code: 401, message: "Email o contraseña incorrecta" };
+    }
+};
+
+const obtenerUsuario = async (email) => {
+    const values = [email];
+    const consulta = "SELECT * FROM usuarios WHERE email = $1";
+    const { rows: [usuario], rowCount } = await pool.query(consulta, values);
+    if (!rowCount) {
+        throw { code: 404, message: "No se encontró ningún usuario con este email" };
+    }
+    return usuario;
+};
+
+module.exports = { registrarUsuario, verificarCredenciales, obtenerUsuario };
