e9697cf65e06fa8ac3d535a780d6a11af2a636ce
RenatoBaeza <rntbzbg@gmail.com>
2024-08-31 20:18:16 -0400

    publication card update

--- 08_proyectofinal/client/src/components/PublicationCard.jsx
+++ 08_proyectofinal/client/src/components/PublicationCard.jsx
@@ -4,16 +4,14 @@ import { Card, Button } from 'react-bootstrap';
 const PublicationCard = ({ publication }) => {
   return (
     <Card style={{ width: '18rem', margin: '10px' }}>
-      <Card.Img variant="top" src={publication.img_url} alt={publication.title} />
+      <Card.Img variant="top" src={publication.img_url} alt={publication.title} className="img-fluid" style={{ aspectRatio: '1', objectFit: 'cover' }} />
       <Card.Body>
         <Card.Title>{publication.title}</Card.Title>
-        <Card.Text>
-          {publication.description}
-        </Card.Text>
-        <Button variant="primary">Status: {publication.status}</Button>
+        <Card.Text>{publication.description}</Card.Text>
+        <Button variant="primary">Ver más</Button>
       </Card.Body>
       <Card.Footer>
-        <small className="text-muted">Published on: {new Date(publication.creation_timestamp).toLocaleDateString()}</small>
+        <Card.Text>Status: {publication.status}</Card.Text>
       </Card.Footer>
     </Card>
   );
